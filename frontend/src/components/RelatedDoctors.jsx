08eb91fc0da1e87c28310b4bfed7376935f41820
shubhii19 <vishwakarmashubhi80@gmail.com>
2025-06-10 12:36:53 +0530

    upto related doctors

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,11 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { useNavigate } from 'react-router-dom';
 
-import { useNavigate } from "react-router-dom";
-import { AppContext } from "../context/AppContext";
+const RelatedDoctors = ({docId , speciality}) => {
+    const {doctors} = useContext(AppContext);
+    const navigate = useNavigate();
+    const [relDocs,setRelDocs] = useState([])
 
-const TopDoctors = () => {
-  const navigate = useNavigate();
-  const { doctors } = useContext(AppContext);
+    useEffect(()=>{
+        if(doctors.length >0 && speciality){
+            const doctorsData = doctors.filter((doc)=>doc.speciality === speciality && doc._id != docId);
+            setRelDocs(doctorsData)
+        }
+    },[doctors,speciality,docId])
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
       <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
@@ -15,7 +22,7 @@ const TopDoctors = () => {
 
       {/* Grid with 5 columns on large screens */}
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 pt-5 gap-y-6 px-3 sm:px-0">
-        {doctors.slice(0, 10).map((item, index) => (
+        {relDocs.slice(0, 5).map((item, index) => (
           <div
             onClick={() => {navigate(`/appointment/${item._id}`);scrollTo(0,0)}}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:-translate-y-2 transition-all duration-500"
@@ -44,7 +51,7 @@ const TopDoctors = () => {
         more
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default TopDoctors;
+export default RelatedDoctors
