dabdeb17c4cf3b0637f5f54d57a3101a0c64e74a
shubhii19 <vishwakarmashubhi80@gmail.com>
2025-07-09 15:02:12 +0530

    appointments fetched for doctor panel

--- admin/src/pages/Doctor/DoctorAppointments.jsx
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -28,20 +28,20 @@ const DoctorAppointments = () => {
           <p>Action</p>
         </div>
         {appointments.map((item,index)=>(
-          <div key={index}>
-            <p>{index+1}</p>
-            <div>
-              <img src={item.userData.image} alt="" /> <p>{item.userData.name}</p>
+          <div className='flex flex-wrap justify-between max-sm:gap-5 max-sm:text-base sm:grid grid-cols-[0.5fr_2fr_1fr_1fr_3fr_1fr_1fr] gap-1 items-center text-gray-500 py-3 px-6 border-b hover:bg-gray-50 ' key={index}>
+            <p className='max-sm:hidden'>{index+1}</p>
+            <div className='flex items-center gap-2'>
+              <img className='w-8 rounded-full' src={item.userData.image} alt="" /> <p>{item.userData.name}</p>
             </div>
-            <div>
+            <div className='text-xs inline border border-[#5F6FFF] px-2 rounded-full'>
               <p>{item.payment ? 'Online':'CASH'}</p>
             </div>
-            <p>{calculateAge(item.userData.dob)}</p>
+            <p className='max-sm:hidden'>{calculateAge(item.userData.dob)}</p>
             <p>{slotDateFormat(item.slotDate)},{item.slotTime}</p>
             <p>{currency}{item.amount}</p>
-            <div>
-              <img src={assets.cancel_icon} alt="" />
-              <img src={assets.tick_icon} alt="" />
+            <div className='flex'>
+              <img className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
+              <img className='w-10 cursor-pointer'  src={assets.tick_icon} alt="" />
             </div>
           </div>
         ))}
