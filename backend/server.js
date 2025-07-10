a20072cab2a8896b7c481b093b10b42d9f4bc410
shubhii19 <vishwakarmashubhi80@gmail.com>
2025-07-10 15:35:42 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,21 @@ connectCloudinary();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+const allowedOrigins = [
+  'http://localhost:5173',        
+  'http://localhost:5174',        
+];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // allow sending cookies/authorization headers
+}));
+
 
 // api endpoint
 
